393825f6295c5e0b8036fabdfdab4e7a571689ba
Jhon <jhon.valdes@softgic.co>
2023-12-21 14:24:38 -0500

    ...

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,7 +2,7 @@ const Post = require('../models/post');
 
 exports.getPosts = async (req, res) => {
   try {
-    const posts = await Post.find().populate('user', 'username'); // 'username' es el campo que quieres recuperar del usuario
+    const posts = await Post.find({ isDeleted: false }).populate('user', 'username'); // 'username' es el campo que quieres recuperar del usuario
     res.json(posts);
   } catch (error) {
     console.error('Error al obtener los posts:', error);
@@ -57,24 +57,25 @@ exports.editPost = async (req, res) => {
 };
 
 exports.deletePost = async (req, res) => {
+  const postId = req.params.id;
   try {
-    const postId = req.params.id;
+    // Actualiza el campo isDeleted en lugar de eliminar físicamente el post
+    const updatedPost = await Post.findById(
+      postId,
+      { isDeleted: true },
+      { new: true } // Devuelve el post actualizado
+    );
 
-    let post = await Post.findById(postId);
-
-    if (!post) {
-      return res.status(404).json({ msg: 'Publicación no encontrada' });
+    if (!updatedPost) {
+      return res.status(404).json({ error: 'Post not found' });
     }
 
-    // Verificar si el usuario es el propietario de la publicación
-    if (post.user.toString() !== req.user.id) {
-      return res.status(401).json({ msg: 'No autorizado para eliminar esta publicación' });
-    }
+    updatedPost.isDeleted = true;
+    await updatedPost.save();
 
-    await post.remove();
-    res.json({ msg: 'Publicación eliminada correctamente' });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Error del servidor');
+    res.json({ message: 'Post marked as deleted', deletedPost: updatedPost });
+  } catch (error) {
+    console.error('Error marking post as deleted:', error);
+    res.status(500).json({ error: 'Error marking post as deleted' });
   }
 };
